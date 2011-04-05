7f243dd76431479b358e2c3b1d401bf2b933c4fa
bjs141@humboldt.edu <bjs141@humboldt.edu@177b4d96-0fa7-350d-81ae-ff863fda7c46>
2011-04-05 05:27:02 +0000

    Added Tests for Location and Observation

--- src/Observation.java
+++ src/Observation.java
@@ -3,6 +3,8 @@
  * 
  * @author  Brannen Sorem
  */  
+ 
+import java.util.Date;
 
 class Observation
 {
@@ -48,12 +50,12 @@ class Observation
     public void   setDate( Date new_date )      { date = new_date;   }
     public void   setQuantity( String quant )   { quantity = quant;  }
     public void   setLocation( Location loc )   { location = loc;    } 
-    public void   setNotes( String notes )      { notes = new_notes; }
+    public void   setNotes( String new_notes )  { notes = new_notes; }
     
     // toString
     public String toString()
     {
-        String str = subject + ', ' + author + ', ' + date.toString();
+        String str = subject + ", " + author + ", " + date.toString();
         return str;
     }
 }
