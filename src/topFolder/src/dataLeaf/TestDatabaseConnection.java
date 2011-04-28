ce78a23a9af53b32c2b5a3cc1220a41b55bad9ae
wrt10@humboldt.edu <wrt10@humboldt.edu@177b4d96-0fa7-350d-81ae-ff863fda7c46>
2011-04-28 15:46:52 +0000

    Updated null verification in DatabaseConnection

--- src/topFolder/src/dataLeaf/TestDatabaseConnection.java
+++ src/topFolder/src/dataLeaf/TestDatabaseConnection.java
@@ -1,18 +1,22 @@
+package dataLeaf;
+
 import java.sql.*;
 
 
 class TestDatabaseConnection {
   
   public static void main(String args[]) {
+
+          DatabaseConnection conn = DatabaseConnection.getInstance(1);
 	  
-	  System.out.println("Instantiated: \t" + DatabaseConnection.getInstance(1).isInstantiated);
+	  System.out.println("Instantiated: \t" + conn.isInstantiated);
 	  //System.out.println(DatabaseConnection.getInstance(1).getConnection());
-	  System.out.println("Driver: \t" + DatabaseConnection.getInstance(1).getDriver());
-	  System.out.println("Connect string: " + DatabaseConnection.getInstance(1).getConnectString());
-	  System.out.println("DB name: \t" + DatabaseConnection.getInstance(1).databaseName);
-	  System.out.println("Active: \t" + DatabaseConnection.getInstance(1).activeConnection);
-	  System.out.println("Get Conn.: \t" + DatabaseConnection.getInstance(1).getConnection());
-	  System.out.println("Connection: \t" + DatabaseConnection.getInstance(1).conn);
+	  System.out.println("Driver: \t" + conn.getDriver());
+	  System.out.println("Connect string: " + conn.getConnectString());
+	  System.out.println("DB name: \t" + conn.databaseName);
+	  System.out.println("Active: \t" + conn.activeConnection);
+	  System.out.println("Get Conn.: \t" + conn.getConnection());
+	  System.out.println("Connection: \t" + conn.conn);
 	  
 	  System.out.println("\n\nend of test");	  
   }
